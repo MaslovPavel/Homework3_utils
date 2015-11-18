e29391d2464c5ab33c9b8ed26c8e7056260ffff1
Maslov Pavel <stavron48@gmail.com>
2015-11-18 03:48:45 +0300

    fixed determinant method, added clone method

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,31 +1,31 @@
 
-public class Matrix {
+public class Matrix implements Cloneable {
     private int rows;
     private int colomns;
     public int[][] matrix;
 
-    private Matrix getMinor(Matrix minor, int row, int column){
-        int minorLength = minor.rows-1;
-        int[][] values = new int[minorLength][minorLength];
-        int dI = 0;
-        int dJ = 0;
+    private Matrix getMinor(Matrix input, int row, int column){
+        int minorLength = input.rows-1;
+        Matrix output = new Matrix(minorLength, minorLength);
+        int passRow = 0;
+        int passCol = 0;
         for(int i = 0; i <= minorLength; i++){
-            dJ = 0;
-            for(int j=0; j<=minorLength; j++){
-                if(i==row){
-                    dI=1;
+            passCol = 0;
+            for(int j = 0; j <= minorLength; j++){
+                if(i == row){
+                    passRow = 1;
                 }
                 else{
-                    if(j==column){
-                        dJ=1;
+                    if(j == column){
+                        passCol = 1;
                     }
                     else{
-                        minor.matrix[i-dI][j-dJ] = matrix[i][j];
+                        output.matrix[i - passRow][j - passCol] = input.matrix[i][j];
                     }
                 }
             }
         }
-        return minor;
+        return output;
     }
 
     public Matrix(int r, int c){
@@ -56,34 +56,43 @@ public class Matrix {
         return this;
     }
 
- /*   public int countDeterminant() {                              Пока не работает =(
-        if (this.rows != this.colomns) {
+
+    int countDeterminant(){
+        if (this.matrix == null || this.rows != this.colomns){
             return 0;
         }
-        Matrix temp = new Matrix(rows, colomns);
-        for (int i = 0; i < rows; i++){
-            for (int j = 0; j < colomns; j++){
+        Matrix temp = new Matrix(this.rows, this.colomns);
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.colomns; j++) {
                 temp.matrix[i][j] = this.matrix[i][j];
             }
         }
         int result = 0;
         if (temp.rows == 2) {
-            result = temp.matrix[0][0] * temp.matrix[1][1] - temp.matrix[1][0] * temp.matrix[0][1];
+            result += temp.matrix[0][0] * temp.matrix[1][1] - temp.matrix[1][0] * temp.matrix[0][1];
         } else {
-            int koeff = 1;
-            for (int i = 0; i < temp.rows; i++) {
+            int coeff = 1;
+            for (int i = 0; i < temp.colomns; i++) {
                 if (i % 2 == 1) {
-                    koeff = -1;
+                    coeff = -1;
                 } else {
-                    koeff = 1;
+                    coeff = 1;
                 }
-                temp = temp.getMinor(temp, 0, 1);
-                result += koeff * this.matrix[0][i] * temp.countDeterminant();
+                result += coeff * temp.matrix[i][0] * temp.getMinor(temp, i, 0).countDeterminant();
             }
         }
         return result;
-    }*/
+    }
 
 
+    public Matrix clone(){
+        Matrix clone = new Matrix(this.rows, this.colomns);
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.colomns; j++) {
+                clone.matrix[i][j] = this.matrix[i][j];
+            }
+        }
+        return clone;
+    }
 
 }
--- test/MatrixTest.java
+++ test/MatrixTest.java
@@ -7,14 +7,9 @@ public class MatrixTest {
     @Test
     public void testCountDeterminant(){
         Matrix test = new Matrix(3, 3);
-        test.setElement(1, 2, 5);
-        test.setElement(2, 0, 1);
-        test.setElement(0, 0, 1);
-        test.setElement(0, 1, 3);
-        test.setElement(0, 2, 1);
-        test.setElement(1, 0, 2);
-        test.setElement(1, 1, 1);
-        test.setElement(2, 2, 1);
-        //assertEquals(test.countDeterminant(), 9);
+        test.matrix=new int[][]{{1, 3, 1},
+                                {2, 1, 5},
+                                {1, 0, 1}};
+        assertEquals(test.countDeterminant(), 9);
     }
 }
